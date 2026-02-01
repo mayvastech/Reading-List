0962cf107000691a66d26805ff29255fb5427afe
mayvastech <65177211+mayvastech@users.noreply.github.com>
2026-02-01 06:06:57 +0000

    Initial commit

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>The Best</Text>
+      <Text>Reading List App</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
