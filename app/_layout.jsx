3993de564efd4e82b05c5e4b58d3d047dde451f6
mayvastech <65177211+mayvastech@users.noreply.github.com>
2026-02-12 05:00:17 +0000

    Added global colour styles to header

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,9 +4,17 @@ import { Stack } from "expo-router";
 
 const RootLayout = () => {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: "#ddd" },
+        headerTintColor: "#333",
+      }}
+    >
       <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="about" options={{title: "About", headerShown: false }} />
+      <Stack.Screen
+        name="about"
+        options={{ title: "About", headerShown: false }}
+      />
       <Stack.Screen name="contact" options={{ title: "Contact" }} />
     </Stack>
   );
