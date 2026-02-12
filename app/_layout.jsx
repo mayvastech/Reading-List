98569a1b170061d38b72898744480eb7b6808045
mayvastech <65177211+mayvastech@users.noreply.github.com>
2026-02-12 04:06:01 +0000

    added stack navigation in layout file

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Stack } from "expo-router";
 
 const RootLayout = () => {
   return (
-    <View style={{ flex: 1 }}>
-      <Text>Footer</Text>
-    </View>
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="about" options={{title: "About", headerShown: false }} />
+      <Stack.Screen name="contact" options={{ title: "Contact" }} />
+    </Stack>
   );
 };
 
